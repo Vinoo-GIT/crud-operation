929bfbf271edf92951dffff579660890f6371f59
jegdi@JegoosPredator <jegdish.jegoo@gmail.com>
2021-11-29 14:54:01 +0530

    header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
-
+import  {Navbar,Container}  from "react-bootstrap";
 
 const Header = () => {
   return <div>
-       <h1>Header</h1>
+       <Navbar fixed="top" bg="dark" variant="dark">
+    <Container fluid>
+      <Navbar.Brand className="navbar" href="">React CRUD</Navbar.Brand>
+    </Container>
+  </Navbar>
        
   </div>
 };
