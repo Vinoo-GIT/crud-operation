c60b2d6d453b16a3ee9bc28819e84f545b3fae4e
jegdi@JegoosPredator <jegdish.jegoo@gmail.com>
2021-11-29 11:48:36 +0530

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { Button } from 'react-bootstrap';
+
+const Header = () => {
+  return <div>
+       Header
+       <Button  variant="primary">Button #1</Button>
+  </div>
+};
+
+export default Header;
