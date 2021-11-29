b3503479b29705c9c9cc93bd212f6bd01f12f896
Jaivi@Vinoo-PC <jaivinodv@outlook.com>
2021-11-29 17:34:42 +0530

    Table Content added

--- src/City.js
+++ src/City.js
@@ -1,7 +1,62 @@
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
+import './Styles/table.css';
+
 const City = () =>{
-  return<div>
-      <h1> City 22</h1>
-  </div>
+  
+return<Container>
+<div><Button id ="add_click_btn" className = "Add_btn" variant="success">Add+</Button>{' '}</div>
+
+<Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>S.no</th>
+      <th>City</th>
+      <th>Actions</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Coimbatore</td>
+      <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+     
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Hosur</td>
+      <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+      
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Salem</td>
+      <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+      
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Chennai</td>
+      <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+      
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Bangalore</td>
+      <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+      
+    </tr>
+  </tbody>
+</Table>
+</Container>
+
 };
 
 export default City;
--- src/Tags.js
+++ src/Tags.js
@@ -1,7 +1,61 @@
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
+import './Styles/table.css';
+
 const Tags = () =>{
-    return<div>
-        <h1 className="Tags-list"> Tags 22</h1>
-    </div>
-  };
+    return<Container>
+    <div><Button id ="add_click_btn" className = "Add_btn" variant="success">Add+</Button>{' '}</div>
+    
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>S.no</th>
+          <th>Tags</th>
+          <th>Actions</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Kia</td>
+          <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+         
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Ford</td>
+          <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+          
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Honda</td>
+          <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+          
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Hyndai</td>
+          <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+          
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>Tata</td>
+          <td> <Button variant="warning">Edit</Button>{' '}
+      <Button variant="danger">Delete</Button></td>
+          
+        </tr>
+      </tbody>
+    </Table>
+    </Container>
+    
+    };
   
   export default Tags;
