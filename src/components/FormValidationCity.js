17cb6e5fb3db06df2acbbc0e9b092da30935a5a3
jegdi@JegoosPredator <jegdish.jegoo@gmail.com>
2021-11-30 17:01:22 +0530

    props added

--- src/components/FormValidationCity.js
+++ src/components/FormValidationCity.js
@@ -1,66 +1,71 @@
-import { Col, Container, Row, Form, Button } from "react-bootstrap"
+import { Col, Container, Row, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import "../index.css";
 
 const FormValidationCity = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
 
-    const onSubmit = (data) => {
-        alert(JSON.stringify(data));
-    };
-
-    return <div className="form_outline">
-        <Container className="form_top">
-            <Row className="form_header">
-                <Col sm={12}>
-                    <h2>Add New City Name</h2>
-                </Col>
-            </Row>
-            <hr></hr>
-            <Row>
-                <Col sm={12}>
-                    <Form onSubmit={handleSubmit(onSubmit)}>
-                        <Form.Group className="mb-3">
-                            <Form.Label className = "City-name-text">City Name :</Form.Label>
-                            <input className ="city-validation-form-text-box"
-                                {...register("city", {
-                                    required: true,
-                                    minLength:2,
-                                    maxLength: 20,
-                                    pattern: /^[A-Za-z]+$/i
-                                })}
-                            />
-                            {errors?.city?.type === "required" && <p>This field is required</p>}
-                            {errors?.city?.type === "maxLength" && (
-                                <p>City name cannot exceed 20 characters</p>
-                            )}
-                            {errors?.city?.type === "minLength" && (
-                                <p>City name cannot be less than 2 characters</p>
-                            )}
-                            {errors?.city?.type === "pattern" && (
-                                <p>Alphabetical characters only</p>
-                            )}
-
-                        </Form.Group>
-                        <Col sm={12} className="form_button">
-                            <Link to="/city">
-                                <Button variant="danger" type="submit">Cancel</Button> </Link>
-                            <Button variant="success" type="submit">Save</Button>
-                        </Col>
-                    </Form>
-                </Col>
-            </Row>
-
-        </Container>
+  return (
+    <div className="form_outline">
+      <Container className="form_top">
+        <Row className="form_header">
+          <Col sm={12}>
+            <h2>Add New City Name</h2>
+          </Col>
+        </Row>
+        <hr></hr>
+        <Row>
+          <Col sm={12}>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group className="mb-3">
+                <Form.Label className="City-name-text">City Name :</Form.Label>
+                <input
+                  className="city-validation-form-text-box"
+                  {...register("city", {
+                    required: true,
+                    minLength: 2,
+                    maxLength: 20,
+                    pattern: /^[A-Za-z]+$/i,
+                  })}
+                />
+                {errors?.city?.type === "required" && (
+                  <p>This field is required</p>
+                )}
+                {errors?.city?.type === "maxLength" && (
+                  <p>City name cannot exceed 20 characters</p>
+                )}
+                {errors?.city?.type === "minLength" && (
+                  <p>City name cannot be less than 2 characters</p>
+                )}
+                {errors?.city?.type === "pattern" && (
+                  <p>Alphabetical characters only</p>
+                )}
+              </Form.Group>
+              <Col sm={12} className="form_button">
+                <Link to="/city">
+                  <Button variant="danger" type="submit">
+                    Cancel
+                  </Button>{" "}
+                </Link>
+                <Button variant="success" type="submit">
+                  Save
+                </Button>
+              </Col>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
-}
-
-
+  );
+};
 
 export default FormValidationCity;
