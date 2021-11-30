a15bf005db18aee74325473a9f0d23f387e7694e
jegdi@JegoosPredator <jegdish.jegoo@gmail.com>
2021-11-30 17:01:45 +0530

    updated

--- src/City.js
+++ src/City.js
@@ -1,69 +1,79 @@
-import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button'
-import Table from 'react-bootstrap/Table'
-import { Link } from 'react-router-dom';
-import './Styles/table.css';
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+import { Link } from "react-router-dom";
+import "./Styles/table.css";
 
+const City = () => {
+  return (
+    <Container>
+      <div>
+        <Link to="/FormValidationCity">
+          <Button id="add_click_btn" className="Add_btn" variant="success">
+            Add+
+          </Button>{" "}
+        </Link>
+      </div>
 
-
-const City = () =>{
-  
-return<Container>
-<div>
-  <Link to="/FormValidationCity">
-  <Button id ="add_click_btn"  className = "Add_btn" variant="success">Add+</Button>{' '}
-  </Link></div>
-
-{/* "/FormValidationCity" */}
-<Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>S.no</th>
-      <th>City</th>
-      <th>Actions</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Coimbatore</td>
-      <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-     
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Hosur</td>
-      <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-      
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Salem</td>
-      <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-      
-    </tr>
-    <tr>
-      <td>4</td>
-      <td>Chennai</td>
-      <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-      
-    </tr>
-    <tr>
-      <td>5</td>
-      <td>Bangalore</td>
-      <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-      
-    </tr>
-  </tbody>
-</Table>
-</Container>
-
+      {/* "/FormValidationCity" */}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>S.no</th>
+            <th>City</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Coimbatore</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Hosur</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Salem</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Chennai</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Bangalore</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default City;
--- src/Tags.js
+++ src/Tags.js
@@ -1,68 +1,78 @@
-import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button'
-import Table from 'react-bootstrap/Table'
-import './Styles/table.css';
-import { Link } from 'react-router-dom';
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+import "./Styles/table.css";
+import { Link } from "react-router-dom";
 
-const Tags = () =>{
-    return<Container>
-    <div>
-      <Link to="/FormValidationTag">
-      <Button id ="click_btn" className = "Add_btn" variant="success">Add+</Button>
-      {' '}
-      </Link>
+const Tags = () => {
+  return (
+    <Container>
+      <div>
+        <Link to="/FormValidationTag">
+          <Button id="click_btn" className="Add_btn" variant="success">
+            Add+
+          </Button>{" "}
+        </Link>
       </div>
-    
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>S.no</th>
-          <th>Tags</th>
-          <th>Actions</th>
-          
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Kia</td>
-          <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-         
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Ford</td>
-          <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-          
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Honda</td>
-          <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-          
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>Hyndai</td>
-          <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-          
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>Tata</td>
-          <td> <Button variant="warning">Edit</Button>{' '}
-      <Button variant="danger">Delete</Button></td>
-          
-        </tr>
-      </tbody>
-    </Table>
-    </Container>
-    
-    };
 
-  
-  export default Tags;
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>S.no</th>
+            <th>Tags</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Kia</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Ford</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Honda</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Hyndai</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Tata</td>
+            <td>
+              {" "}
+              <Button variant="warning">Edit</Button>{" "}
+              <Button variant="danger">Delete</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default Tags;
