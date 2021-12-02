4d42b4347c4b5bff55c28b7530085abf6b9dd1ea
jegdi@JegoosPredator <jegdish.jegoo@gmail.com>
2021-12-02 15:32:02 +0530

    context added

---
+++ src/context/GlobalState.js
@@ -0,0 +1,24 @@
+import React, {Children, createContext,useReducer} from 'react';
+import AddReducer from "./AppReducer";
+
+const initialState ={
+    users:[
+        {id:1,name:"Kia"},
+        {id:2,name:"Ford"},
+        {id:3,name:"Honda"},
+        {id:4,name:"Hyundai"},
+        {id:5,name:"Tata"}
+    ]
+};
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({children}) => {
+    const[state,dispatch] = useReducer(AppReducer,initialState);
+    return(
+        <GlobalContext.Provider value={{
+            users:state.users
+        }}>
+        {children}
+        </GlobalContext.Provider>
+    )
+    }
