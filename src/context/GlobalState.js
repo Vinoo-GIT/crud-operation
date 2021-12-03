3957c608782496c4425ec9f7ccfabc462864fcc7
Jegdish Kumar <jegdish.jegoo@gmail.com>
2021-12-03 10:36:35 +0530

    tags updated

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,24 +1,28 @@
-import React, {Children, createContext,useReducer} from 'react';
-import AddReducer from "./AppReducer";
+import React, { Children, createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
-const initialState ={
-    users:[
-        {id:1,name:"Kia"},
-        {id:2,name:"Ford"},
-        {id:3,name:"Honda"},
-        {id:4,name:"Hyundai"},
-        {id:5,name:"Tata"}
-    ]
+const initialState = {
+  users: [
+    { id: 1, name: "Kia" },
+    { id: 2, name: "Ford" },
+    { id: 3, name: "Honda" },
+    { id: 4, name: "Hyundai" },
+    { id: 5, name: "Tata" },
+  ],
 };
 export const GlobalContext = createContext(initialState);
 
-export const GlobalProvider = ({children}) => {
-    const[state,dispatch] = useReducer(AppReducer,initialState);
-    return(
-        <GlobalContext.Provider value={{
-            users:state.users
-        }}>
-        {children}
-        </GlobalContext.Provider>
-    )
-    }
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  return (
+    <GlobalContext.Provider
+      value={{
+        users: state.users,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalProvider;
